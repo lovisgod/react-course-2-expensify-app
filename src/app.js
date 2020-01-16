524f55579ed2100f803c8db2e42be0005f92055b
Lovisgod <olifedayo94@gmail.com>
2020-01-16 16:09:35 +0100

    Ft/create routers

--- src/app.js
+++ src/app.js
@@ -1,45 +1,63 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import AppRouter, { history } from './routers/AppRouter';
-import configureStore from './store/configureStore';
-import { startSetExpenses } from './actions/expenses';
-import { login, logout } from './actions/auth';
-import getVisibleExpenses from './selectors/expenses';
+import {BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom'
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
-import 'react-dates/lib/css/_datepicker.css';
-import { firebase } from './firebase/firebase';
-import LoadingPage from './components/LoadingPage';
 
-const store = configureStore();
-const jsx = (
-  <Provider store={store}>
-    <AppRouter />
-  </Provider>
+const ExpenseDashBoardPage = () => (
+  <div>
+      This is from my dashboard component
+  </div>
 );
-let hasRendered = false;
-const renderApp = () => {
-  if (!hasRendered) {
-    ReactDOM.render(jsx, document.getElementById('app'));
-    hasRendered = true;
-  }
-};
 
-ReactDOM.render(<LoadingPage />, document.getElementById('app'));
+const CreateExpensePage = () => (
+    <div>
+        This is from my expense component
+    </div>
+  );
+  const EditExpensePage = () => (
+    <div>
+        This is from my edit page
+    </div>
+  );
+  const HelpPage = () => (
+    <div>
+        This is from my help page
+    </div>
+  );
 
-firebase.auth().onAuthStateChanged((user) => {
-  if (user) {
-    store.dispatch(login(user.uid));
-    store.dispatch(startSetExpenses()).then(() => {
-      renderApp();
-      if (history.location.pathname === '/') {
-        history.push('/dashboard');
-      }
-    });
-  } else {
-    store.dispatch(logout());
-    renderApp();
-    history.push('/');
-  }
-});
+  const NotFoundPage = () => (
+    <div>
+        404! - <Link to="/">Go home</Link>
+    </div>
+  );
+
+  const Header = () => (
+    <header>
+      <h1>Expensify</h1>
+      <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
+      <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
+      <NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink>
+      <NavLink to="/help" activeClassName="is-active">Help</NavLink>
+    </header>
+  );
+
+  // switch moves through the routes and stops when there is a match
+  // then if there are no match then it will continue moving till it get to the bottom
+  // and finds the notfound page
+const routes = (
+    <BrowserRouter>
+    <div>
+      <Header/>
+      <Switch>
+      <Route path= '/' component={ExpenseDashBoardPage} exact={true}/>
+      <Route path= '/create' component={CreateExpensePage}/>
+      <Route path= '/edit' component={EditExpensePage}/>
+      <Route path= '/help' component={HelpPage}/>
+      <Route component={NotFoundPage}/>
+    </Switch>
+    </div>
+    </BrowserRouter>
+)
+
+ReactDOM.render(routes, document.getElementById('app'));
